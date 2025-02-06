dd514969e5985d5b9551cad9b4d01633b2a22fde
toobi-jpeg <rex.98@live.se>
2025-02-06 20:01:57 +0100

    localstorage added & design updates

--- F25D Uppgift 2/js/themes.js
+++ F25D Uppgift 2/js/themes.js
@@ -1,10 +1,40 @@
-const themeSwitch = document.getElementById("theme-switch");
-
-themeSwitch.addEventListener("click", () => {
-  document.body.classList.toggle("lightmode");
-});
-
 function hamburgerMenu() {
   const menu = document.querySelector(".hamburger-menu");
   menu.classList.toggle("show");
+}
+
+const themeSwitch = document.getElementById("theme-switch");
+
+let savedTheme = localStorage.getItem("mode");
+if (savedTheme) {
+  document.body.classList.add(savedTheme);
+} else {
+  document.body.classList.add("lightmode");
+}
+updateIcons();
+
+themeSwitch.addEventListener("click", () => {
+  if (document.body.classList.contains("lightmode")) {
+    document.body.classList.remove("lightmode");
+    document.body.classList.add("darkmode");
+    localStorage.setItem("mode", "darkmode");
+  } else {
+    document.body.classList.remove("darkmode");
+    document.body.classList.add("lightmode");
+    localStorage.setItem("mode", "lightmode");
+  }
+  updateIcons();
+});
+
+function updateIcons() {
+  const moonIcon = document.querySelector("#theme-switch svg:first-child");
+  const sunIcon = document.querySelector("#theme-switch svg:last-child");
+
+  if (document.body.classList.contains("darkmode")) {
+    moonIcon.style.display = "none";
+    sunIcon.style.display = "block";
+  } else {
+    moonIcon.style.display = "block";
+    sunIcon.style.display = "none";
+  }
 }
